c9533bd999af40be4695b8b3142fca3dd5c447a7
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-10-05 19:14:07 +0300

    Moving declaration of constants in the App

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,11 +1,3 @@
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
 const Header = (props) => {
 return(
 <>
@@ -28,9 +20,9 @@ const Part = (props) => {
 const Content = (props) => {
   return(
   <>
-   <Part part = {part1} exercises = {exercises1} />
-   <Part part = {part2} exercises = {exercises2} />
-   <Part part = {part3} exercises = {exercises3} />
+   <Part part = {props.part1} exercises = {props.exercises1} />
+   <Part part = {props.part2} exercises = {props.exercises2} />
+   <Part part = {props.part3} exercises = {props.exercises3} />
   </>
   )
 }
@@ -46,10 +38,17 @@ const Total = (props) => {
 }
 
 const App = () => { 
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3 = {exercises3}/>
       <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
     </div>
   )
