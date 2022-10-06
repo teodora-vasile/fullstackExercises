56d325f512ac8347b1fb3f63de79f4a1ad48d6e7
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-10-06 16:54:22 +0300

    Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,43 +6,44 @@ return (<button onClick = {handleClick}>
 </button>)
 }
 
-const Statistics = ({text, number}) => {
+const StatisticLine = ({text, number}) => (<p>{text}: {number}</p>)
+
+const Statistics = ({good, neutral, bad, all}) => {
 return (
-<p>{text}: {number}</p>
+  <>
+<h1>Statistics:</h1>
+<StatisticLine text = "good" number = {good} />
+<StatisticLine text = "neutral" number = {neutral} />
+<StatisticLine text = "bad" number = {bad} />
+<StatisticLine text = "all" number = {all} />
+<StatisticLine text = "average" number = {(good-bad)/all} />
+<StatisticLine text = "positive" number = {good*100/all} />
+</>
 )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-const handleGoodClick = () => {
+  const handleGoodClick = () => {
 setGood(good + 1)
 }
-const handleNeutralClick = () => {
+  const handleNeutralClick = () => {
 setNeutral(neutral+1)
 }
-const handleBadClick = () => {
+  const handleBadClick = () => {
 setBad(bad+1)
 }
-const all = good + neutral + bad
-
+  const all = good + neutral + bad
   return (
-    <div>
+    <>
       <h1>Give feedback for Cafeteria</h1>
       <Button handleClick = {handleGoodClick} text = "good" />
       <Button handleClick = {handleNeutralClick} text = "neutral" />
       <Button handleClick = {handleBadClick} text = "bad" />
-      <h1>Statistics:</h1>
-<Statistics text = "good" number = {good} />
-<Statistics text = "neutral" number = {neutral} />
-<Statistics text = "bad" number = {bad} />
-<Statistics text = "all" number = {all} />
-<Statistics text = "average" number = {(good-bad)/all} />
-<Statistics text = "positive" number = {good*100/all} />
-
-    </div>
+      <Statistics good ={good} neutral ={neutral} bad = {bad} all={all}/>
+    </>
   )
 }
 
