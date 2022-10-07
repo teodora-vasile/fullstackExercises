3a96c51850fa8637dd4b1ce363617689a4de2cee
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-10-07 16:25:22 +0300

    Exercise 2.4

--- part2/courseinformation/src/App.js
+++ part2/courseinformation/src/App.js
@@ -17,7 +17,7 @@ const Total = ({parts}) => {
   const total = parts.reduce((sum, currentValue) =>
   sum + currentValue.exercises, 0)
   return(
-    <p>Total of {total} exercises </p>
+    <p style = {{fontWeight: "bold"}}>Total of {total} exercises </p>
   )
 }
 
@@ -31,30 +31,57 @@ const Course = ({course}) => {
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+  const App = () => {
+    const courses = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
       {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
       }
     ]
-  }
 
-  return <Course course={course} />
+  return(
+    <>
+    {courses.map(course =>  <Course course={course} key = {course.id}/>)}
+  </>
+  )
 }
 
 export default App
