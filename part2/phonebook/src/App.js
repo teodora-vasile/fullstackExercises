2012fb30a88bb205a72411f0d09446ae8f5b0495
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-10-08 21:57:36 +0300

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Person = ({person}) => <p>{person.name} {person.number}</p>
 
@@ -39,15 +40,18 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newPerson, setNewPerson] = useState({name:'', number:''})
   const [search, setSearch] = useState('')
   const [listToShow, setListToShow] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then (response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handlePersonChange = (event) => {
   setNewPerson({...newPerson, [event.target.id]: event.target.value})
